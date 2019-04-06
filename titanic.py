f8a74aec9844fcf7e04b2701b0c936fd1ce9c53e
krishghiya <44220584+krishghiya@users.noreply.github.com>
2019-04-05 18:50:33 -0700

    Add files via upload

--- titanic.py
+++ titanic.py
@@ -5,43 +5,91 @@ Created on Wed Oct 24 13:22:54 2018
 @author: kghiy
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
+#import numpy as np
 import pandas as pd
 
 train_set = pd.read_csv("train.csv")
 test_set = pd.read_csv("test.csv")
 
 #dataset = pd.concat([train_set, test_set])
+#train_set.info()
+#train_set[['Fare', 'Survived']].groupby(['Fare'],
+#        as_index=False).mean().sort_values(by='Survived', ascending=False)
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#import seaborn as sns
+
+# =============================================================================
+# sns.FacetGrid(train_set, col='Survived').map(plt.hist, 'Pclass', bins=20)
+
+#Map multiple features
+
+#grid = sns.FacetGrid(train_set, col='Survived', row='PClass', height=2.2, aspect=1.6)
+#grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+#grid.add_legend();
+# =============================================================================
+
+#Categorize Names
+
+for dataset in [train_set, test_set]:
+    
+    dataset["Title"] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',
+ 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+    
+#    dataset['Title'] = dataset['Title'].map({"Mr": 1, "Miss": 2, "Mrs": 3, 
+#           "Master": 4, "Rare": 5})
+    
+#    Add Family - Sum of siblings and parents
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+    
+#    Fill NaN values of Embarked
+    dataset['Embarked'] = dataset['Embarked'].fillna(dataset['Embarked'].mode())
+    dataset['Age'] = dataset['Age'].fillna(dataset['Age'].median())
+
+#View frequency
+
+#pd.crosstab(train_set['Title'], train_set['Sex'])
 
 #One hot encoding the "embarked" column for both train and test sets
+    
+from sklearn.preprocessing import LabelEncoder
  
-train_set = pd.get_dummies(train_set, columns=['Embarked'], drop_first=False)
-test_set = pd.get_dummies(test_set, columns=['Embarked'], drop_first=False)
+train_set = pd.get_dummies(train_set, columns=['Embarked', 'Title'])
+test_set = pd.get_dummies(test_set, columns=['Embarked', 'Title'])
 
 label_encoder = LabelEncoder()
-train_set.iloc[:, 4] = label_encoder.fit_transform(train_set.iloc[:, 4])
-test_set.iloc[:, 3] = label_encoder.fit_transform(test_set.iloc[:, 3])
+train_set['Sex'] = label_encoder.fit_transform(train_set['Sex'])
+test_set['Sex'] = label_encoder.fit_transform(test_set['Sex'])
 
 #Removing non numeric columns as they cant be compared
 
-train_set = train_set.select_dtypes(exclude=['object'])
-test_set = test_set.select_dtypes(exclude=['object'])
+#train_set = train_set.select_dtypes(exclude=['object'])
+#test_set = test_set.select_dtypes(exclude=['object'])
+train_set = train_set.drop(['Name', 'Ticket', 'Cabin', 'Parch', 'SibSp'], axis=1)
+test_set = test_set.drop(['Name', 'Ticket', 'Cabin', 'Parch', 'SibSp'], axis=1)
 
-#from sklearn.metrics import mean_absolute_error
-
-from sklearn.preprocessing import Imputer
-
-#Filling NaN values with mean
-
-imputer = Imputer(strategy="mean")
-
-train_set["Age"] = imputer.fit_transform(train_set["Age"].values.reshape(-1,1))
-test_set["Age"] = imputer.fit_transform(test_set["Age"].values.reshape(-1,1))
+#One missing in Fare
 
 test_set = test_set.fillna(test_set.median())
+
+#Grouping Fare
+for dataset in [train_set, test_set]:
+    dataset['FareBin'] = pd.qcut(dataset['Fare'], 5)
+    label = LabelEncoder()
+    dataset['FareBin'] = label.fit_transform(dataset['FareBin'])
+    
+    dataset['AgeBin'] = pd.qcut(dataset['Age'], 4)
+    dataset['AgeBin'] = label.fit_transform(dataset['AgeBin'])  
+
+train_set = train_set.drop(['Fare'], axis=1)
+test_set = test_set.drop(['Fare'], axis=1)
+
+train_set = train_set.drop(['Age'], axis=1)
+test_set = test_set.drop(['Age'], axis=1)
 
 #Combining the sibling and parent columns
 
@@ -53,13 +101,31 @@ X_train = train_set.iloc[:, train_set.columns != 'Survived']
 y_train = train_set["Survived"]
 X_test = test_set.iloc[:, test_set.columns != 'Survived']
 
+#Standardizing the values
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+train_set = scaler.fit_transform(train_set)
+test_set = scaler.fit_transform(test_set)
+
 from sklearn.ensemble import RandomForestClassifier
 
 #Fitting the model
 
-reg = RandomForestClassifier()
-reg.fit(X_train, y_train)
-y_test = reg.predict(X_test)
+#reg = RandomForestClassifier(n_estimators=100)
+#reg.fit(X_train, y_train)
+#y_test = reg.predict(X_test)
+
+#from sklearn.metrics import mean_absolute_error
+
+from xgboost import XGBClassifier
+
+my_model = XGBClassifier(n_estimators=70)
+my_model.fit(X_train, y_train, verbose=False)
+
+# make predictions
+y_test = my_model.predict(X_test)
 
 #Writing to csv
 
